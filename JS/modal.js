59755013fe59fd631279e4d8a1896afafc6cc609
StephenSchooley <schooley.sa@gmail.com>
2021-03-23 20:31:00 -0400

    Updated JS

--- JS/modal.js
+++ JS/modal.js
@@ -1,16 +1,22 @@
 const modalForm = document.querySelector('form');
-const modalContainer = document.getElementByClass(".modalContainer");
+const modalContainer = document.querySelector('.modalContainer');
 const header = document.querySelector('header');
+
 
 
 modalForm.addEventListener('submit', function(e) {
     e.preventDefault();
-}
+    modalContainer.style.display = "none";
+    header.style.opacity = "1";
+    const userName = document.getElementById('modalName').value
+    console.log(userName);}
+    
 );
 
 window.onclick = function(e) {
-    if (e.target == modalForm) {
+      if (e.target.className!== "modalInput") {
+      console.log(e.target.className)
       modalContainer.style.display = "none";
-      header.opacity= "1";
+      header.style.opacity = "1";
     }
-}
+};
